59aeb96ba03d41587566c495f34e80601a0b9ec4
Iqbal Fauzi <iqbalfawz@gmail.com>
2024-12-27 07:09:40 +0700

    Fix for newer php version

--- modules/lib-mail/library/Mail.php
+++ modules/lib-mail/library/Mail.php
@@ -13,19 +13,22 @@ class Mail
 {
 	private static $handler;
 
-	private static function applyParams(string $text, array $params): string{
-		foreach($params as $key => $val)
-			$text = str_replace('(:'.$key.')', $val, $text);
+	private static function applyParams(string $text, array $params): string
+	{
+		foreach ($params as $key => $val) {
+			$text = str_replace('(:'.$key.')', $val ?? '', $text);
+		}
 		return $text;
 	}
 
-	private static function buildParams($params, string $parent=''): array{
+	private static function buildParams($params, string $parent=''): array
+	{
 		$result = [];
-		foreach($params as $par => $val){
-			if(is_array($val) || is_object($val)){
+		foreach ($params as $par => $val) {
+			if (is_array($val) || is_object($val)) {
 				$res = self::buildParams($val, $parent . $par . '.');
 				$result = array_merge($result, $res);
-			}else{
+			} else {
 				$result[$parent.$par] = $val;
 			}
 		}
@@ -33,12 +36,14 @@ class Mail
 		return $result;
 	}
 
-	private static function getHandler(): ?string{
-		if(self::$handler)
+	private static function getHandler(): ?string
+	{
+		if (self::$handler) {
 			return self::$handler;
+		}
 
 		$config = (object)\Mim::$app->config->libMail;
-		if(!isset($config->handler) || !$config->handler){
+		if (!isset($config->handler) || !$config->handler) {
 			throw new \Exception('No email handler registered');
 			return null;
 		}
@@ -46,28 +51,32 @@ class Mail
 		return $config->handler;
 	}
 
-    private static function validateOptions(array $options): bool{
+    private static function validateOptions(array $options): bool
+    {
         $req_fields = ['to','subject'];
-        foreach($req_fields as $fld){
-            if(!isset($options[$fld]))
+        foreach ($req_fields as $fld) {
+            if (!isset($options[$fld])) {
                 throw new \Exception('Property `' . $fld . '` is required');
+            }
         }
 
         return true;
     }
 
-    static function send(array $options): bool {
+    static function send(array $options): bool
+    {
     	$params = $options['view']['params'] ?? [];
     	$params = $params ? self::buildParams($params) : [];
 
-        if(!self::validateOptions($options))
+        if (!self::validateOptions($options)) {
             return false;
+        }
 
     	$recipients = $options['to'];
 
     	$handler = self::getHandler();
 
-    	foreach($recipients as $recipient){
+    	foreach ($recipients as $recipient) {
     		$params['to.name']  = $recipient['name'];
     		$params['to.email'] = $recipient['email'];
 
@@ -79,24 +88,27 @@ class Mail
     			'html' 		=> ''
     		];
 
-    		if($fopts['text'])
+    		if ($fopts['text']) {
     			$fopts['text'] = self::applyParams($fopts['text'], $params);
+    		}
 
-    		if(isset($options['view'])){
+    		if (isset($options['view'])) {
     			$view              = $options['view'];
     			$view_params       = $view['params'] ?? [];
     			$view_params['to'] = $recipient;
     			$fopts['html']     = View::render($view['path'], $view_params, 'mail');
     		}
 
-    		if(!$handler::send($fopts))
+    		if (!$handler::send($fopts)) {
     			return false;
+    		}
     	}
 
     	return true;
     }
 
-    static function lastError(): ?string {
+    static function lastError(): ?string
+    {
     	$handler = self::getHandler();
     	return $handler::lastError();
     }
